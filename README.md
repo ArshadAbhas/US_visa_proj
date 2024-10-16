08fcab38150c8fd41cb42da4756e2060a2fde198
ArshadAbhas <arshadabhas123@gmail.com>
2024-10-16 21:41:47 +0530

    Requiremenst.txt is changes

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # US_visa_proj
 #yokoso 
+#pip install -r requirements.txt
---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup, find_packages
+setup(
+    name="us_visa",
+    version="0.0.0",
+    author="Arshad Ahmed",
+    author_email="vamousarshad@gmail.com",
+    packages=find_packages()
+)
